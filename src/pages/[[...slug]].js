ddc38a109686ef807a2fedd019f4b8912f2b2842
Rrylprnc <122914831+Rrylprnc@users.noreply.github.com>
2025-07-22 10:58:02 -0400

    Update [[...slug]].js

--- src/pages/[[...slug]].js
+++ src/pages/[[...slug]].js
@@ -22,7 +22,7 @@ function Page(props) {
     return (
         <>
             <Head>
-                <title>{title}</title>
+                <title>{Premier Stays}</title>
                 {metaDescription && <meta name="description" content={metaDescription} />}
                 {metaTags.map((metaTag) => {
                     if (metaTag.format === 'property') {
